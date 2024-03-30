af6348f22161bceda22564834bc7f0189f561688
gamal <ag3204123@gmail.com>
2024-03-30 02:21:21 +0200

    finish user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -14,7 +14,7 @@ export const follow = async (req, res) => {
     }
 
     // CHECK THE TWO USERS ARE DIFFERENT
-    if (currentUser._id === userToFollowId) {
+    if (currentUser._id.toString() === userToFollowId) {
       return res.status(400).json({ message: "You cannot follow yourself" });
     }
 
@@ -51,7 +51,7 @@ export const unfollow = async (req, res) => {
     }
 
     // CHECK THE TWO USERS ARE DIFFERENT
-    if (currentUser._id === userToUnfollowId) {
+    if (currentUser._id.toString() === userToUnfollowId) {
       return res.status(400).json({ message: "You cannot unfollow yourself" });
     }
 
@@ -68,7 +68,7 @@ export const unfollow = async (req, res) => {
     );
 
     userToUnfollow.followers = userToUnfollow.followers.filter(
-      (id) => id !== currentUser._id,
+      (id) => id !== currentUser._id.toString(),
     );
 
     await Promise.all([currentUser.save(), userToUnfollow.save()]);
@@ -76,6 +76,45 @@ export const unfollow = async (req, res) => {
     res.status(200).json({ message: "User unfollowed successfully" });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
-    console.log("user controller - follow error:", error.message);
+    console.log("user controller - unfollow error:", error.message);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { password, name, username, email, profilePic, bio } = req.body;
+    const user = req.user;
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+
+    user.name = name || user.name;
+    user.username = username || user.username;
+    user.email = email || user.email;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+
+    await user.save();
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.log("user controller - updateUser error:", error.message);
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.log("user controller - getUserProfile error:", error.message);
   }
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { follow, unfollow } from "../controllers/user.controller.js";
+import {
+  follow,
+  getUserProfile,
+  unfollow,
+  updateUser,
+} from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/follow/:id", isAuthenticated, follow);
 userRouter.post("/unfollow/:id", isAuthenticated, unfollow);
+userRouter.put("/update-user", isAuthenticated, updateUser);
+userRouter.get("/profile/:id", getUserProfile);
 
 export default userRouter;
